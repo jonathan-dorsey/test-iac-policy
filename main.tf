5463279d8efa10c17f00d6536d8562a1fc88b019
Jono Dorsey <jono.dorsey@exchange.boeing.com>
2024-05-21 14:48:03 -0400

    Initial code

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.1"
+    }
+  }
+}
+
+resource "random_string" "randomname" {
+  length  = 16
+  count   = 2
+  special = false
+  upper   = false
+}
